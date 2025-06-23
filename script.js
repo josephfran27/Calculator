147a3ac36890e9b4667a33a13b2575710bfe37d3
Joseph France <jafcxm@umsystem.edu>
2025-06-23 17:32:58 -0500

    Touched up some inconsistencies.

--- script.js
+++ script.js
@@ -1,4 +1,93 @@
 
+
+//DOM elements
+const resultText = document.querySelector('.resultText');
+const numBtns = document.querySelectorAll('.number');
+const operatorBtns = document.querySelectorAll('.operator');
+const clearBtn = document.querySelector('.clear');
+const deleteBtn = document.querySelector('.delete');
+const equalsBtn = document.querySelector('.equals');
+
+//values
+let currentInput = '';
+let inputOne = '';
+let operator = '';
+let inputTwo = '';
+
+//result display
+function displayVal(value) {
+    resultText.textContent = value;
+}
+
+//number input
+numBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.textContent;
+        if(currentInput === '0' && value !== '.') {
+            currentInput = value;
+        }
+        else {
+            currentInput += button.textContent;
+        }
+        displayVal(currentInput);
+    });
+});
+
+//operator input
+operatorBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        if(currentInput === '') {
+            return;
+        } 
+        if(inputOne !== '' && operator !== '') {
+            inputTwo = currentInput;
+            let result = operation(parseFloat(inputOne), operator, parseFloat(inputTwo));
+            inputOne = result.toString();
+            displayVal(result);
+        }
+        else {
+            inputOne = currentInput;
+        }
+        operator = button.textContent;
+        currentInput = '';
+    });
+});
+
+//equals button
+equalsBtn.addEventListener('click', () => {
+    if(inputOne === '' || currentInput == '') {
+        return;
+    } 
+    inputTwo = currentInput;
+    let result = operation(parseFloat(inputOne), operator, parseFloat(inputTwo));
+    displayVal(result);
+    currentInput = result.toString();
+    inputOne = '';
+    operator = '';
+    inputTwo ='';
+});
+
+//clear button
+clearBtn.addEventListener('click', () => {
+    currentInput = '';
+    inputOne = '';
+    operator = '';
+    inputTwo ='';
+    displayVal('0');
+})
+
+//delete button
+deleteBtn.addEventListener('click', () => {
+    currentInput = currentInput.slice(0, -1);   //start at beginning and remove last value
+    if(currentInput == '') {
+        displayVal('0');
+    }
+    else if(currentInput != ('0')) {
+        displayVal(currentInput);
+    }
+});
+
+
 //OPERATIONS
 function add(a, b) {
     return a + b;
@@ -37,82 +126,3 @@ function operation(inputOne, operator, inputTwo) {
         return;
     }
 }
-
-//DOM elements
-const resultText = document.querySelector('.resultText');
-const numBtns = document.querySelectorAll('.number');
-const operatorBtns = document.querySelectorAll('.operator');
-const clearBtn = document.querySelector('.clear');
-const deleteBtn = document.querySelector('.delete');
-const equalsBtn = document.querySelector('.equals');
-
-//values
-let currentInput = '';
-let inputOne = '';
-let operator = '';
-let inputTwo = '';
-
-//result display
-function displayVal(value) {
-    resultText.textContent = value;
-}
-
-//number input
-numBtns.forEach(button => {
-    button.addEventListener('click', () => {
-        currentInput += button.textContent;
-        displayVal(currentInput);
-    });
-});
-
-//operator input
-operatorBtns.forEach(button => {
-    button.addEventListener('click', () => {
-        if (currentInput === '') {
-            return;
-        } 
-        if (inputOne !== '' && operator !== '') {
-            inputTwo = currentInput;
-            let result = operation(parseFloat(inputOne), operator, parseFloat(inputTwo));
-            inputOne = result.toString();
-            displayVal(result);
-        }
-        else {
-            inputOne = currentInput;
-        }
-        operator = button.textContent;
-        currentInput = '';
-    });
-});
-
-equalsBtn.addEventListener('click', () => {
-    if(inputOne === '' || currentInput == '') {
-        return;
-    } 
-    inputTwo = currentInput;
-    let result = operation(parseFloat(inputOne), operator, parseFloat(inputTwo));
-    displayVal(result);
-    currentInput = result.toString();
-    inputOne = '';
-    operator = '';
-    inputTwo ='';
-});
-
-clearBtn.addEventListener('click', () => {
-    currentInput = '';
-    inputOne = '';
-    operator = '';
-    inputTwo ='';
-    displayVal('0');
-})
-
-deleteBtn.addEventListener('click', () => {
-    currentInput = currentInput.slice(0, -1);   //start at beginning and remove last value
-    if(currentInput == '') {
-        displayVal('0');
-    }
-    else if(currentInput != ('0')) {
-        displayVal(currentInput);
-    }
-});
-
